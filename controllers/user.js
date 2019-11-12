4954ded35f611631fbd5fad8dfdae76bf7e42174
Tanmay Jain <tanmayjain53@gmail.com>
2019-11-12 09:48:20 +0530

    Added signin and signout

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const  jwt =  require("jsonwebtoken");
+const expressJwt = require( "express-jwt");
 const {errorHandler} = require("../helpers/dberrorHandler");
 
 exports.signup = (req,res)=>{
@@ -6,7 +8,7 @@ const user = new User(req.body);
 user.save((err, user)=>{
     if(err){
         return res.status(400).json({
-            err:errorHandler(err)
+            error:errorHandler(err)
         })
     }
     user.salt = undefined;
@@ -15,4 +17,32 @@ user.save((err, user)=>{
         user
     })
 })
+}
+
+exports.signin = (req,res) =>{
+    const {email,password} = req.body;
+
+    User.findOne({email}, (err,user)=>{
+        if(err || !user){
+            return res.status(401).json({
+                error:"User with that email does not exist. Please signup"
+            });
+        }
+
+        if(!user.authenticate(password)){
+            return res.json({
+                error:"Email and password don't match"
+            })
+        }
+        const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET)
+        res.cookie("t",token,{expire:new Date()+9999})
+
+        const {_id,name,email,role} = user;
+        return res.json({token, user:{_id,email,name,role}})
+    })
+}
+
+exports.signout = (req,res) =>{
+    res.clearCookie("t");
+    res.json({message:"Signout Success"});
 }
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const {userSignupValidator} = require("../validator")
-const {signup} = require("../controllers/user");
+const {signup, signin, signout} = require("../controllers/user");
 
 router.post("/signup",userSignupValidator,signup);
+router.post("/signin",signin);
+router.get("/signout",signout);
 
 module.exports = router;
