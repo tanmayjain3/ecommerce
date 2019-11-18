40d12ebdbf92722934df0796a7927d8bb43a9e8d
Tanmay Jain <tanmayjain53@gmail.com>
2019-11-18 10:19:52 +0530

    Added product model

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 const app = express();
 
 mongoose.connect(process.env.DATABASE,{
@@ -25,6 +26,7 @@ app.use(expressValidator());
 app.use("/api",authRoutes);
 app.use("/api",userRoutes);
 app.use("/api",categoryRoutes);
+app.use("/api",productRoutes);
 const port = process.env.PORT||8000;
 
 app.listen(port,()=>{
---
+++ controllers/product.js
@@ -0,0 +1,68 @@
+const formidable = require("formidable");
+const _ = require("lodash");
+const Product  = require("../models/product");
+const fs = require("fs");
+const errorHandler = require("../helpers/dberrorHandler");
+
+exports.productById = (req,res,next, id) =>{
+    Product.findById(id).exec((err,product)=>{
+        if(err || !product){
+            return res.status(400).json({
+                error:"Product not found"
+            })
+        }
+        req.product = product;
+        next();
+    })
+}
+
+exports.read = (req,res)=>{
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
+
+exports.create = (req,res)=>{
+
+    let form = new formidable.IncomingForm();
+     form.keepExtensions = true;
+     form.parse(req,(err,fields,files) =>{
+        if(err){
+            return res.status(400).json({
+                error:"Image could not be uploaded"
+            })
+        }
+
+        const {name, description, quantity, price, photo, category, shipping} = fields;
+        if(!name || !description || !quantity || !price || !photo || !category || !shipping){
+            return res.status(400).json({
+                error:"All fields are required"
+            })
+        }
+        let product = new Product(fields)
+   
+        if(files.photo){
+
+            if(files.photo.size){
+                return res.status(400).json({
+                    error:"Image should be less than 1mb in size"
+                })  
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type;
+        }
+   
+        product.save((err,result)=>{
+           if(err){
+               return res.status(400).json({
+                   error:errorHandler(err)
+               })
+           }
+   
+           res.json(result);
+   
+        })
+     })
+
+
+}
